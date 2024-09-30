202bda3090c8ddf006d52e4ddfb822de5ebd466e
jiamid <jiamid.t@binarywalk.com>
2024-10-01 00:29:17 +0800

    del doubao

--- tg_bot/handlers/messages.py
+++ tg_bot/handlers/messages.py
@@ -13,7 +13,6 @@ from aiogram.types import Message
 from tg_bot.bot import telegram_router
 from commonts.util import to_escape_string
 from commonts.storage_manager import proxy_manager
-from commonts.doubao import DouBaoBot
 import re
 
 
@@ -44,36 +43,3 @@ async def cmd_start(message: Message) -> None:
                          f'\n`/set_proxy 代理区域 代理链接`'
                          f'\n`/del_proxy 代理区域`'
                          f'\n\n', parse_mode='MarkdownV2')
-
-
-@telegram_router.message(F.text == "echo")
-async def echo(message: types.Message) -> None:
-    try:
-        await message.send_copy(chat_id=message.chat.id)
-    except Exception as e:
-        logger.error(f"Can't send message - {e}")
-        await message.answer("Nice try!")
-
-
-@telegram_router.message(F.text == "ping")
-async def hello(message: types.Message) -> None:
-    try:
-        await message.answer("pong")
-    except Exception as e:
-        logger.error(f"Can't send message - {e}")
-        await message.answer("Nice try!")
-
-
-@telegram_router.message(F.text.contains("画"))
-async def handle_draw(message: Message):
-    get_flag = await message.reply('Get')
-    prompt = message.text
-    doubao_bot = DouBaoBot()
-    result = await doubao_bot.text_to_img(prompt)
-    img = result.get('url')
-    tips = result.get('tips')
-    if img:
-        await message.reply_photo(img)
-    if tips:
-        await message.reply(tips)
-    await get_flag.delete()
